18e6629270e441ee653bae643b17d5793a4cf178
gabrieleggiusti <giusti98@gmail.com>
2022-04-05 15:16:50 +0200

    First commit

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+/*
+Chiedi all’utente:
+1) Il suo nome;
+2) il suo cognome;
+3) il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito22
+*/
+
+const firstName = prompt ('Inserisci il tuo nome');
+
+const lastName = prompt ('Inserisci il tuo cognome');
+
+const color = prompt ('Inserisci il tuo colore preferito');
+
+
+console .log (firstName);
+
+console .log (lastName);
+
+console .log (color);
+
